3362733f315e3151e91569cdcdaf8948c8237e84
Matous Melecky <melecmat@fel.cvut.cz>
2021-08-30 22:26:33 +0200

    Major overhaul. New page and models are moved to MRS.

--- bash/compile3DApp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#script to use to compile pug and filenames
-cd /mnt/c/users/matou/Documents/GitHub/3D-models/
-python3 bash/save_filenames_into_json.py
-handlebars templates -m --output compiled_templates.js
